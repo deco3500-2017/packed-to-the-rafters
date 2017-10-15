d6fb91e7666f1f35bcaa4057e1bfc2e9b1fc408f
Coleen McKay <coleen.mckay@hotmail.com>
2017-10-15 21:51:25 +1000

    Added create event page and the JS to toggle it

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
+	/* Redirects to map page */
     $("#login").on("click", function() {
     	window.location.href = "map.html";
+    });
+
+    /* Shows create event div */
+    $(".add-event").on("click", function() {
+    	$(".create-event").slideToggle();
+    });
+
+    /* Cancels create event */
+    $("#cancel").on("click", function() {
+    	$(".create-event").slideToggle();
     });
 });
